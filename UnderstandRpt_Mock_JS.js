195703ca74565d9211be8f1c9790dfb13f760e6e
CarterVE <caeitreim@gmail.com>
2018-08-29 17:01:28 -0600

    - Removed extraneous code from CSS and JS - Removed scrollbar hidden option from CSS file - Removed Search Input field code from CSS file - Removed .active from CSS file (no idea what it did) - Removed general .scrolled-nav class, all other classes have scrolled-nav subclasses
    + Added comments to CSS and JS files

--- UnderstandRpt_Mock_JS.js
+++ UnderstandRpt_Mock_JS.js
@@ -1,5 +1,5 @@
 $(window).scroll(function () {
-    if ($("#nav-dropdown-content").css('display') == 'block'){
+    if ($("#nav-dropdown-content").css('display') == 'block'){  //Stops sticky nav collapse if Contents is displayed (prevents everything jumping around)
         return;
     }
     else if ($(document).scrollTop() <= 40) {
@@ -7,7 +7,6 @@ $(window).scroll(function () {
     } 
     else {
         $('#main-nav').addClass('scrolled-nav');        // Changes different stylings based on user scrolling down past 40px
-        $('.dropdown-content').css("display","none");   // Hides initially displayed dropdown if user scrolls page at all
     } 
 }).one('scroll', function() {
     $('.dropdown-content').css("display","none");   // Hides initially displayed dropdown if user scrolls page at all
@@ -17,65 +16,3 @@ $(window).scroll(function () {
 $(document).one('click', function() {
   $('.dropdown-content').css("display","none");
 });
-
-//var isHovered = $("#dropdown-button").is(":hover");
-
-
-/**
-
-var body = document.body;
-
-var dropdownDisp = function() {
-if( $("#nav-dropdown-content").css('display') == 'block') {
-    //$('#main-content').addClass('noscroll');
-    $('#main-content').css('overflow', 'hidden');
-    $('#main-content').css('position', 'relative');
-}
-else {
-    $('#main-content').css('overflow', 'auto');
-    $('#main-content').css('position', 'relative');
-}
-};
-
-setInterval(dropdownDisp, 1000);
-
-
-
-
-
-
-if ($('main-dropdown:hover').length !== 0) {
-    alert('hi');
-}
-
-function isHover(e) {
-    return (e.parentElement.querySelector(':hover') === e);
-}
-
-
-var navContent = document.getElementById('main-dropdown');
-document.addEventListener('mousemove', function checkHover() {
-    var hovered = isHover(navContent);
-    if (hovered) {
-        alert('hi')
-    }
-
-});
-
-setInterval(function() {
-    if ($('#nav-dropdown-content:hover')) {
-    //alert($(".dropdown-content").scrollTop());
-        $("#nav-dropdown-content").css("background", "yellow");
-        //$('#main-content').addClass('noscroll');
-    }
-    else {
-        $("#nav-dropdown-content").css("background", "aaa");
-    }
-}, 1);
-
-
-$(".dropdown .dropdown-content").mouseout(function() {
-    $("#nav-dropdown-content").scrollTop();
-});
-
-**/

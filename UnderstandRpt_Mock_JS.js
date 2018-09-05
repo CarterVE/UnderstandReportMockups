55715ca8dfbed833056a72cf321fc679e9e59c47
Carter <caeitreim@gmail.com>
2018-09-05 15:23:28 -0600

    Added auto collapse to JS file for navbar on ToC release, and slight formatting modifications to CSS file for very thin windows

--- UnderstandRpt_Mock_JS.js
+++ UnderstandRpt_Mock_JS.js
@@ -1,5 +1,11 @@
 $(window).scroll(function () {
     if ($("#nav-dropdown-content").css('display') == 'block'){  //Stops sticky nav collapse if Contents is displayed (prevents everything jumping around)
+        if ($(document).scrollTop() > 40) {
+            sessionStorage.setItem('navbarScrollHold', 'expanded');    //Stores variable to tell page navbar is expanded (but should be collapsed)
+        }
+        else if ($(document).scrollTop() <= 40) {
+            sessionStorage.setItem('navbarScrollHold', 'collapsed');    //Stores variable to tell page navbar is collapsed (but should be expanded)
+        }
         return;
     }
     else if ($(document).scrollTop() <= 40) {
@@ -12,6 +18,17 @@ $(window).scroll(function () {
     $('.dropdown-content').css("display","none");   // Hides initially displayed dropdown if user scrolls page at all
     }); 
 
+// Function checks every .2s to see if navbar is being held and can now be collapsed/expanded (respectively)
+setInterval(function() {
+    if (sessionStorage.getItem('navbarScrollHold') == 'expanded' && $("#nav-dropdown-content").css('display') == 'none') {
+        $('#main-nav').addClass('scrolled-nav');
+        sessionStorage.setItem('navbarScrollHold', 'no');
+    }
+    else if (sessionStorage.getItem('navbarScrollHold') == 'collapsed' && $("#nav-dropdown-content").css('display') == 'none') {
+        $('#main-nav').removeClass('scrolled-nav');
+        sessionStorage.setItem('navbarScrollHold', 'no');
+    }
+}, 200);
 
 // Makes the dropdown which is displayed when page is first opened disapper after clicking anywhere
 $(document).one('click', function() {
